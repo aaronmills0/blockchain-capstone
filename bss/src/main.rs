59accc73206da3d74374d9cfcecd26726592f885
Aidan Jackson <aidan.jackson@mail.mcgill.ca>
2022-10-15 17:43:47 -0400

    Removed Unnecessary Vec

--- bss/src/main.rs
+++ bss/src/main.rs
@@ -21,14 +21,13 @@ fn main() {
 
     loop {
         display_utxo(&utxo);
-        let (senders, receivers, units, contribution) = add_transaction();
+        let (senders, receivers, units) = add_transaction();
         if !update_transaction(
             &senders,
             &receivers,
             &units,
-            &contribution,
             &mut transaction_list,
-            &mut utxo,
+            &mut utxo
         ) {
             continue;
         }
@@ -72,54 +71,24 @@ fn display_utxo(utxo: &HashMap<String, u128>) {
     println!();
 }
 
-fn add_transaction() -> (Vec<String>, Vec<String>, Vec<u128>, Vec<u128>) {
+fn add_transaction() -> (Vec<String>, Vec<String>, Vec<u128>) {
     println!("New transaction:\n");
 
     let mut senders: Vec<String> = Vec::new();
     let mut receivers: Vec<String> = Vec::new();
     let mut units: Vec<u128> = Vec::new();
-    let mut contribution: Vec<u128> = Vec::new();
     let mut user_input = String::new();
 
-    loop {
-        //Receive and Proccess user Input-Output-Unit Pairs
-        println!("Please Enter the Sender-Contribution Pair as Follows, 'a 10':");
-        user_input.clear();
-        io::stdin()
-            .read_line(&mut user_input)
-            .expect("Failed to read line");
-        
-        let split = user_input.split_whitespace(); //Tokenize by whitespace
-        let s2 = split.clone();
-        if s2.count() != 2{
-            continue;
-        }
-
-        let mut i = 0;
-        for s in split{ //Note next_chunk() not yet functional so for loop is needed
-            if i % 2 == 0{
-                senders.push(s.trim().to_string());
-            }
-            else{
-                let unit: u128 = match s.trim().parse() {
-                    Ok(num) => num,
-                    Err(_) => process::exit(1),
-                };
-                contribution.push(unit);
-            }
-            i += 1;
-        }
-
-        println!("Would you like to add another sender-contribution pair? [y/n]:");
-        user_input.clear();
-        io::stdin()
-            .read_line(&mut user_input)
-            .expect("Failed to read line");
-
-        match user_input.trim() {
-            "y" => {}
-            _ => break,
-        }
+    //Receive and Proccess user Input-Output-Unit Pairs
+    println!("Please Enter the Senders as Follows, 'a b c ...':");
+    user_input.clear();
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read line");
+    
+    let split = user_input.split_whitespace(); //Tokenize by whitespace
+    for s in split{
+        senders.push(s.trim().to_string());
     }
 
     loop{
@@ -162,32 +131,31 @@ fn add_transaction() -> (Vec<String>, Vec<String>, Vec<u128>, Vec<u128>) {
         }
     }
 
-    return (senders, receivers, units, contribution);
+    return (senders, receivers, units);
 }
 
 fn update_transaction(
     senders: &Vec<String>,
     receivers: &Vec<String>,
     units: &Vec<u128>,
-    contribution: &Vec<u128>,
     transaction_list: &mut Vec<Transaction>,
     utxo: &mut HashMap<String, u128>,
 ) -> bool {
 
-    //Check if senders are contributing enough
-    let c_sum: u128 = contribution.iter().sum();
     let u_sum: u128 = units.iter().sum();
-
-    if u_sum > c_sum {
-        println!("Invalid Transaction!");
-        return false;
-    }
-
-    for (i, s) in senders.iter().enumerate(){
-        if !(utxo.contains_key(s)) || contribution[i] > *utxo.get(s).unwrap(){
+    let mut s_sum: u128 = 0;
+    for s in senders{
+        if !(utxo.contains_key(s)){
             println!("Invalid transaction!\n");
             return false;
         }
+        else{
+            s_sum += utxo.get(s).unwrap();
+        }
+    }
+    if u_sum > s_sum{
+        println!("Invalid transaction!\n");
+        return false;
     }
 
     let transaction: Transaction = Transaction {
@@ -221,7 +189,7 @@ fn update_transaction(
         }
     }
 
-    let fee: u128 = c_sum - u_sum;
+    let fee: u128 = s_sum - u_sum;
     println!("Transaction fee: {}\n", &fee);
     return true;
 }
