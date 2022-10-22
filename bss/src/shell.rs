11af35fd1fe0d35a34c593f8dffcbd5206e7d379
samiad2788 <52794206+sami270@users.noreply.github.com>
2022-10-22 18:18:42 -0300

    added comments to clarify what is going on in shell exit

--- bss/src/shell.rs
+++ bss/src/shell.rs
@@ -41,13 +41,19 @@ pub fn shell() -> bool {
         },
         "exit" | "Exit" | "EXIT" => {
             info!("The user selected exit");
+            //cwd, cwdFrom, cwdTo, cwdLog will allow us to access the path to the current directory
             let cwd = std::env::current_dir().unwrap();
             let cwdFrom = std::env::current_dir().unwrap();
             let cwdTo = std::env::current_dir().unwrap();
             let cwdLog = std::env::current_dir().unwrap();
+
+            //dirpath will allow us to access the path where we will store the new log file
             let mut dirpath = cwd.into_os_string().into_string().unwrap();
+            //dirpathFrom will allow us to access the path of the orginal log file we copy from
             let mut dirpathFrom = cwdFrom.into_os_string().into_string().unwrap();
+            //dirpathTo will allow us to access the path of the log file we copy into
             let mut dirpathTo = cwdTo.into_os_string().into_string().unwrap();
+            //dirpathFrom will allow us to access the path of the orginal log file we copy from after we moved dirPathFrom
             let mut dirpathLog = cwdLog.into_os_string().into_string().unwrap();
 
             dirpath.push_str("/log");
@@ -58,11 +64,13 @@ pub fn shell() -> bool {
             
             let dir_path=Path::new(&dirpath);
             let n1=Local::now().format("%Y-%m-%d-%H-%M-%S").to_string();
+            //The new log file contains the current time
             let filename1:&str=&format!("sam{}.log",n1);
             dirpathTo.push_str(filename1);
             let file_path = dir_path.join(filename1);
             let file = File::create(file_path);
             let copied = fs::copy(dirpathFrom, dirpathTo);
+            //we remove the old log file
             let log_file = File::create(&dirpathLog).unwrap();
             
             exit_program();
