87e2156d72c9b8edfd18536bc60302d1a2b65604
samiad2788 <52794206+sami270@users.noreply.github.com>
2022-10-31 12:12:12 -0300

    fixed path for finding config file

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,9 @@ fn main() {
     let cwd = std::env::current_dir().unwrap();
     let mut cwd_string = cwd.into_os_string().into_string().unwrap();
     if env::consts::OS == "windows" {
-        cwd_string.push_str("\\src\\logging_config.yaml");
+        cwd_string.push_str("\\logging_config.yaml");
     } else {
-        cwd_string.push_str("/src/logging_config.yaml");
+        cwd_string.push_str("/logging_config.yaml");
     }
     log4rs::init_file(cwd_string, Default::default()).unwrap();
 
