0a5ff49a26b85f87e9f9fa8482860893cc750395
samiad2788 <52794206+sami270@users.noreply.github.com>
2023-01-21 14:29:55 -0400

    removed get_headers

--- src/network/get_data.rs
+++ src/network/get_data.rs
@@ -5,7 +5,8 @@ use serde::{Deserialize, Serialize};
 pub struct GetData {
     pub message_header: MessageHeader,
     pub count: u32,
+    // Valid types: 0. Block Header Hash, 1. Transaction Header Hash, 2. Full Block, 3. Full Transaction
     pub data_type: u8,
-    // send last has of header of last block/transaction in sender blockchain
+    // Send last has of header of last block/transaction in sender blockchain
     pub starting_header_hash: String,
 }
--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,5 +1,4 @@
 mod block_headers;
 mod get_data;
-mod get_headers;
 mod inv;
 mod message_header;
