0fd4bb07742491f9c047f6f9cd50d354bcd4476f
Aaron Mills <74072722+aaronmills0@users.noreply.github.com>
2023-02-02 14:19:24 -0500

    Transactions message creation and decode. Compacted message creation by creating a function to generate headers

--- src/network/messages.rs
+++ src/network/messages.rs
@@ -4,90 +4,92 @@ use bytes::Bytes;
 use mini_redis::Frame;
 use serde_json;
 
-pub fn get_peerid_query() -> Frame {
-    let command = String::from("00000000");
-    let peerid_sender = String::from("0".repeat(32));
-    let peerid_receiver = String::from("0".repeat(31) + "1");
+use crate::components::transaction::Transaction;
 
-    let mut header = command.clone();
-    header.push_str(&peerid_sender);
-    header.push_str(&peerid_receiver);
+pub fn get_header(sourceid: u32, destid: u32, command: String) -> Frame {
+    let peerid_source_unprocessed = format!("{sourceid:#034b}");
+    let peerid_dest_unprocessed = format!("{destid:#034b}");
+
+    let mut peerid_source = String::new();
+    if let Some(part) = peerid_source_unprocessed.get(2..) {
+        peerid_source = part.to_string();
+    }
+
+    let mut peerid_dest = String::new();
+    if let Some(part) = peerid_dest_unprocessed.get(2..) {
+        peerid_dest = part.to_string();
+    }
+
+    let mut header = command;
+    header.push_str(&peerid_source);
+    header.push_str(&peerid_dest);
 
     let header_bytes = Bytes::from(header);
 
-    let wrapper_header = Frame::Bulk(header_bytes);
-    return Frame::Array(Vec::from([wrapper_header]));
+    let header_frame = Frame::Bulk(header_bytes);
+
+    return header_frame;
+}
+
+pub fn get_peerid_query() -> Frame {
+    let header_frame = get_header(0, 1, String::from("00000000"));
+    return Frame::Array(Vec::from([header_frame]));
 }
 
 // notation for functions that return message type is get_name_response()
-pub fn get_peerid_response(next_peerid: u32) -> Frame {
+pub fn get_peerid_response(destid: u32) -> Frame {
     let mut response_vec: Vec<Frame> = Vec::new();
 
-    let command = String::from("00000001");
-    let peerid_sender = String::from("0".repeat(31) + "1");
-    let peerid_receiver_unprocessed = format!("{next_peerid:#034b}");
-    let mut peerid_receiver = String::new();
-    if let Some(part) = peerid_receiver_unprocessed.get(2..) {
-        peerid_receiver = part.to_string();
+    let header_frame = get_header(1, destid, String::from("00000001"));
+    response_vec.push(header_frame);
+
+    let peerid_dest_unprocessed = format!("{destid:#034b}");
+    let mut peerid_dest = String::new();
+    if let Some(part) = peerid_dest_unprocessed.get(2..) {
+        peerid_dest = part.to_string();
     }
 
-    let mut header = command.clone();
-    header.push_str(&peerid_sender);
-    header.push_str(&peerid_receiver);
-
-    let header_bytes = Bytes::from(header);
-
-    let wrapper_header = Frame::Bulk(header_bytes);
-    response_vec.push(wrapper_header);
-
-    let peerid = Frame::Bulk(Bytes::from(peerid_receiver));
+    let peerid = Frame::Bulk(Bytes::from(peerid_dest));
     response_vec.push(peerid);
 
     return Frame::Array(response_vec);
 }
 
-pub fn get_sockets_query(peerid: u32, socket: String) -> Frame {
-    let command = String::from("00000010");
-    let peerid_sender_unprocessed = format!("{peerid:#034b}");
-    let mut peerid_sender = String::new();
-    if let Some(part) = peerid_sender_unprocessed.get(2..) {
-        peerid_sender = part.to_string();
-    }
-    let peerid_receiver = String::from("0".repeat(31) + "1");
-
-    let mut header = command.clone();
-    header.push_str(&peerid_sender);
-    header.push_str(&peerid_receiver);
-
-    let header_bytes = Bytes::from(header);
+pub fn get_sockets_query(sourceid: u32, socket: String) -> Frame {
+    let header_frame = get_header(sourceid, 1, String::from("00000010"));
     let payload_bytes = Bytes::from(socket);
-
-    let wrapper_header = Frame::Bulk(header_bytes);
-    let wrapper_payload = Frame::Bulk(payload_bytes);
-    return Frame::Array(Vec::from([wrapper_header, wrapper_payload]));
+    let payload_frame = Frame::Bulk(payload_bytes);
+    return Frame::Array(Vec::from([header_frame, payload_frame]));
 }
 
-pub fn get_sockets_response(socketmap: HashMap<u32, String>, receiver_id: u32) -> Frame {
+pub fn get_sockets_response(socketmap: HashMap<u32, String>, destid: u32) -> Frame {
     let mut response_vec: Vec<Frame> = Vec::new();
 
-    let command = String::from("00000011");
-    let peerid_sender = String::from("0".repeat(31) + "1");
-    let peerid_receiver_unprocessed = format!("{receiver_id:#034b}");
-    let mut peerid_receiver = String::new();
-    if let Some(part) = peerid_receiver_unprocessed.get(2..) {
-        peerid_receiver = part.to_string();
-    }
-
-    let mut header = command.clone();
-    header.push_str(&peerid_sender);
-    header.push_str(&peerid_receiver);
-
-    let header_bytes = Bytes::from(header);
-
-    let wrapper_header = Frame::Bulk(header_bytes);
-    response_vec.push(wrapper_header);
+    let header_frame = get_header(1, destid, String::from("00000011"));
+    response_vec.push(header_frame);
 
     let payload = Frame::Bulk(Bytes::from(serde_json::to_string(&socketmap).unwrap()));
+    response_vec.push(payload);
+    return Frame::Array(response_vec);
+}
+
+pub fn get_termination_msg(sourceid: u32, destid: u32) -> Frame {
+    let mut response_vec: Vec<Frame> = Vec::new();
+
+    let header_frame = get_header(sourceid, destid, String::from("00000100"));
+
+    response_vec.push(header_frame);
+
+    return Frame::Array(response_vec);
+}
+
+pub fn get_transaction_msg(sourceid: u32, destid: u32, tx: Transaction) -> Frame {
+    let mut response_vec: Vec<Frame> = Vec::new();
+
+    let header_frame = get_header(sourceid, destid, String::from("00000101"));
+    response_vec.push(header_frame);
+
+    let payload = Frame::Bulk(Bytes::from(serde_json::to_string(&tx).unwrap()));
     response_vec.push(payload);
     return Frame::Array(response_vec);
 }
