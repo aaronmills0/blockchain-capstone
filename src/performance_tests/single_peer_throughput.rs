a7139519680856878159a1f745f6f08cc258eade
roeywine <roeywine@gmail.com>
2023-02-08 15:52:04 -0500

    InitialTestFramework: Add Test to Shell
    - Add test flag to shell for transactions

--- src/performance_tests/single_peer_throughput.rs
+++ src/performance_tests/single_peer_throughput.rs
@@ -1,26 +1,28 @@
 use crate::network::messages;
-use crate::network::peer::{self, Peer};
+use crate::network::peer;
 use crate::shell::get_example_transaction;
 use log::info;
+use std::collections::HashMap;
 use std::thread::sleep;
 use std::time::Duration;
 
 static mut counter: u32 = 0;
 
-async fn test_single_peer_tx_throughput_sender(
-    sending_peer: &Peer,
+pub async fn test_single_peer_tx_throughput_sender(
+    sending_id: u32,
+    ip_map: HashMap<u32, String>,
+    ports_map: HashMap<String, Vec<String>>,
     receiving_id: u32,
     duration: u64,
 ) {
     let sleep_time = Duration::from_micros(duration);
-    let sending_peerid = sending_peer.peerid;
-    let receiving_ip = sending_peer.ip_map.get(&receiving_id).unwrap();
-    let receiving_ports = sending_peer.port_map.get(&receiving_ip.to_owned()).unwrap();
+    let receiving_ip = ip_map.get(&receiving_id).unwrap();
+    let receiving_ports = ports_map.get(&receiving_ip.to_owned()).unwrap();
 
     let mut sender_counter: u32 = 0;
     loop {
         let frame =
-            messages::get_transaction_msg(sending_peerid, receiving_id, get_example_transaction());
+            messages::get_transaction_msg(sending_id, receiving_id, get_example_transaction());
         peer::send_transaction(frame, receiving_ip.to_owned(), receiving_ports.to_owned()).await;
 
         sender_counter += 1;
