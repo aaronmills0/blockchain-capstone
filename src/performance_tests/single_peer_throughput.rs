4faadb081ec7da9b19a1958319ac023cadcbd51c
roeywine <roeywine@gmail.com>
2023-02-14 21:20:55 -0500

    InitialTestFramework: Update Mempool, Manager Get
    - Update mempool to have a hashset of tx hashes and a vector of
    transactions
    - Create a verified mempool to store all transactions that have
    been verified (in parallel) by a receiver peer
    - Change names to sender and receiver in single_peer_throughput.rs

--- src/performance_tests/single_peer_throughput.rs
+++ src/performance_tests/single_peer_throughput.rs
@@ -7,27 +7,27 @@ use std::thread::sleep;
 use std::time::Duration;
 
 pub async fn test_single_peer_tx_throughput_sender(
-    sending_id: u32,
+    sender_id: u32,
     ip_map: HashMap<u32, String>,
     ports_map: HashMap<String, Vec<String>>,
-    receiving_id: u32,
+    receiver_id: u32,
     duration: u64,
 ) {
     let sleep_time = Duration::from_micros(duration);
-    let receiving_ip = ip_map.get(&receiving_id).unwrap();
-    let receiving_ports = ports_map.get(&receiving_ip.to_owned()).unwrap();
+    let receiver_ip = ip_map.get(&receiver_id).unwrap();
+    let receiver_ports = ports_map.get(&receiver_ip.to_owned()).unwrap();
 
-    let ports: Vec<&str> = receiving_ports.iter().map(AsRef::as_ref).collect();
-    let connection_opt = get_connection(receiving_ip, ports.as_slice()).await;
+    let ports: Vec<&str> = receiver_ports.iter().map(AsRef::as_ref).collect();
+    let connection_opt = get_connection(receiver_ip, ports.as_slice()).await;
     if connection_opt.is_none() {
-        panic!("Cannot connect to the receiving peer to send a transaction (test)");
+        panic!("Cannot connect to the receiver peer to send a transaction (test)");
     }
     let mut connection = connection_opt.unwrap();
 
     let mut sender_counter: u32 = 0;
     loop {
         let frame =
-            messages::get_transaction_msg(sending_id, receiving_id, get_example_transaction());
+            messages::get_transaction_msg(sender_id, receiver_id, get_example_transaction());
         connection.write_frame(&frame).await.ok();
 
         sender_counter += 1;
